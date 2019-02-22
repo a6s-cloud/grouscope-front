a73c785368b081ad7c836aee592217a233eac47a
鈴木 直登 <nsuzuki7713@gmail.com>
2019-02-22 22:14:11 +0900

    add study page

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,10 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Tsutomu from './views/Tsutomu.vue';
+import Toki from './views/Toki.vue';
+import Minokich from './views/Minokich.vue';
+import Naoto from './views/Naoto.vue';
 
 Vue.use(Router);
 
@@ -20,6 +24,26 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+    },
+    {
+      path: '/tsutomu',
+      name: 'tsutomu',
+      component: Tsutomu,
+    },
+    {
+      path: '/toki',
+      name: 'toki',
+      component: Toki,
+    },
+    {
+      path: '/minokich',
+      name: 'minokich',
+      component: Minokich,
+    },
+    {
+      path: '/naoto',
+      name: 'naoto',
+      component: Naoto,
     },
   ],
 });

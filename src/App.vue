2203a4aeada55c8f39063e2b008a2efdcd3d31c7
nsuzuki7713 <nsuzuki7713@gmail.com>
2019-04-21 21:32:58 +0900

    動作確認用に追加

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@
     <div style="margin-top:8px;">
       <el-button @click="$router.push({ name: 'analysisRequest' })">解析依頼画面</el-button>
       <el-button @click="$router.push({ name: 'analysisResultList' })">解析結果一覧画面</el-button>
-      <el-button @click="$router.push({ name: 'analysisResult' })">解析結果画面</el-button>
+      <el-button @click="$router.push({ path: '/analysisResult/1' })">解析結果画面</el-button>
     </div>
     <router-view></router-view>
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->

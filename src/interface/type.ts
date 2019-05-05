422896e731af018f7d48a5dac5580e49284ade3d
nsuzuki7713 <nsuzuki7713@gmail.com>
2019-05-05 12:52:04 +0900

    interfaceを使用してデータを反映

--- src/interface/type.ts
+++ src/interface/type.ts
@@ -1,22 +1,41 @@
 export interface IAnalysisResult {
-  analysisEndDate: string
-  analysisStartDate: string
-  analysisWord: string
-  deleteFlag: number
-  favoriteCount: number
-  id: number
-  image: string
-  insertDate: string
-  retweetCount: number
-  status: number
-  tweetCount: number
-  updateDate: string
-  url: string
-  userCount: number
-  userRanking: IUserRanking[]
+  analysisEndDate: string;
+  analysisStartDate: string;
+  analysisWord: string;
+  deleteFlag: number;
+  favoriteCount: number;
+  id: number;
+  image: string;
+  insertDate: string;
+  retweetCount: number;
+  status: number;
+  tweetCount: number;
+  updateDate: string;
+  url: string;
+  userCount: number;
+  userRanking: IUserRanking[];
 }
 
-interface IUserRanking {
+// AnalysisResultの初期化処理
+export const initAnalysisResult: IAnalysisResult  = {
+  analysisEndDate: '',
+  analysisStartDate: '',
+  analysisWord: '',
+  deleteFlag: 0,
+  favoriteCount: 0,
+  id: 0,
+  image: '',
+  insertDate: '',
+  retweetCount: 0,
+  status: 0,
+  tweetCount: 0,
+  updateDate: '',
+  url: '',
+  userCount: 0,
+  userRanking: []
+}
+
+export interface IUserRanking {
   tweetCount: number;
   userAccount: string;
   userName: string;
--- src/views/AnalysisResult.vue
+++ src/views/AnalysisResult.vue
@@ -4,11 +4,11 @@
       <span>Word Cloudの画像</span>
       <el-col>
         <el-card :body-style="{ padding: '0px' }" style="width:940px;margin:0 auto;">
-          <img :src="imagePath(image)" class="image" />
+          <img :src="imagePath(analysisResult.image)" class="image" />
           <div style="padding: 14px;">
-            <span>{{analysisWord}}</span>
+            <span>{{analysisResult.analysisWord}}</span>
             <div class="bottom clearfix">
-              <time class="time">{{startDate}} 〜 {{endDate}}</time>
+              <time class="time">{{analysisResult.analysisStartDate}} 〜 {{analysisResult.analysisEndDate}}</time>
             </div>
           </div>
         </el-card>
@@ -30,25 +30,25 @@
         <el-col :span="6">
           <el-card class="box-card">
             <div class="text item">いいね数</div>
-            <div class="text item">{{favoriteCount}}</div>
+            <div class="text item">{{analysisResult.favoriteCount}}</div>
           </el-card>
         </el-col>
         <el-col :span="6">
           <el-card class="box-card">
             <div class="text item">リツート数</div>
-            <div class="text item">{{retweetCount}}</div>
+            <div class="text item">{{analysisResult.retweetCount}}</div>
           </el-card>
         </el-col>
         <el-col :span="6">
           <el-card class="box-card">
             <div class="text item">ツイート数</div>
-            <div class="text item">{{tweetCount}}</div>
+            <div class="text item">{{analysisResult.tweetCount}}</div>
           </el-card>
         </el-col>
         <el-col :span="6">
           <el-card class="box-card">
             <div class="text item">ユーザ数</div>
-            <div class="text item">{{userCount}}</div>
+            <div class="text item">{{analysisResult.userCount}}</div>
           </el-card>
         </el-col>
       </el-row>
@@ -74,54 +74,40 @@ import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 import axios from 'axios';
 import { ANALYSIS_RESULT } from '../mockdata/testData';
+import { IAnalysisResult, IUserRanking, initAnalysisResult } from '../interface/type';
 
 export default Vue.extend({
   data: function() {
     return {
-      image: undefined as any,
-      startDate: '2019-04-17 00:00:00' as any,
-      endDate: '2019-04-17 23:59:59' as any,
-      analysisWord: '#技術書典' as any,
-      tweetCount: '872' as any,
-      favoriteCount: '489' as any,
-      userCount: '849' as any,
-      retweetCount: '3518' as any,
-      tableUserRank: [
-        {
-          no: '1',
-          name: 'なおと(@naoto_7713)',
-          tweet: '150ツイート'
-        },
-        {
-          no: '2',
-          name: 'なおと2(@naoto_7713)',
-          tweet: '150ツイート'
-        },
-        {
-          no: '3',
-          name: 'なおと3(@naoto_7713)',
-          tweet: '150ツイート'
-        },
-        {
-          no: '4',
-          name: 'なおと4(@naoto_7713)',
-          tweet: '150ツイート'
-        }
-      ] as any
+      analysisResult: initAnalysisResult as IAnalysisResult
     };
   },
+
   mounted: function() {
     this.getAnalysisResult();
   },
+
+  computed: {
+    tableUserRank: function() {
+      return this.analysisResult.userRanking.map((val: IUserRanking, index: number) => {
+        return {
+          no: index,
+          name: `${val.userName}(@${val.userAccount})`,
+          tweet: `${val.tweetCount}ツイート`
+        };
+      });
+    }
+  },
+
   methods: {
-    imagePath: function(image: any) {
+    imagePath: function(image: string) {
       if (image === 'dummy') {
         return require('@/assets/naoto/result.png');
       }
-
-      if (image === undefined) {
+      if (image === '') {
         return;
       }
+
       return process.env.VUE_APP_API_URL_BASE_STORAGE + image;
     },
     getAnalysisResult: function() {
@@ -130,39 +116,10 @@ export default Vue.extend({
       axios
         .get(endpoint, { timeout: 5000 })
         .then(response => {
-          this.image = response.data.image;
-          this.startDate = response.data.analysisStartDate;
-          this.endDate = response.data.analysisEndDate;
-          this.analysisWord = response.data.analysisWord;
-          this.tweetCount = response.data.tweetCount;
-          this.favoriteCount = response.data.favoriteCount;
-          this.userCount = response.data.userCount;
-          this.retweetCount = response.data.retweetCount;
-          this.tableUserRank = response.data.userRanking.map((val: any, index: any) => {
-            return {
-              no: index,
-              name: `${val.userName}(@${val.userAccount})`,
-              tweet: `${val.tweetCount}ツイート`
-            };
-          });
+          this.analysisResult = response.data;
         })
         .catch(error => {
           // テストデータを反映
-          this.image = ANALYSIS_RESULT.image;
-          this.startDate = ANALYSIS_RESULT.analysisStartDate;
-          this.endDate = ANALYSIS_RESULT.analysisEndDate;
-          this.analysisWord = ANALYSIS_RESULT.analysisWord;
-          this.tweetCount = ANALYSIS_RESULT.tweetCount;
-          this.favoriteCount = ANALYSIS_RESULT.favoriteCount;
-          this.userCount = ANALYSIS_RESULT.userCount;
-          this.retweetCount = ANALYSIS_RESULT.retweetCount;
-          this.tableUserRank = ANALYSIS_RESULT.userRanking.map((val: any, index: any) => {
-            return {
-              no: index,
-              name: `${val.userName}(@${val.userAccount})`,
-              tweet: `${val.tweetCount}ツイート`
-            };
-          });
           (this as any).$notify.warning({
             title: 'warning',
             message: 'テストデータを反映しました。'
@@ -186,6 +143,7 @@ export default Vue.extend({
   }
 });
 </script>
+
 <style scoped>
 .home {
   width: 1200px;

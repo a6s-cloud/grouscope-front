9cf64affd56ab179ceb85dd8d2d1c8a77e139b52
nsuzuki7713 <nsuzuki7713@gmail.com>
2019-05-06 12:20:29 +0900

    不要ファイル削除

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
-import Tsutomu from './views/Tsutomu.vue';
-import Toki from './views/Toki.vue';
-import Minokich from './views/Minokich.vue';
-import Naoto from './views/Naoto.vue';
 import AnalysisRequest from './views/AnalysisRequest.vue';
 import AnalysisResultList from './views/AnalysisResultList.vue';
 import AnalysisResult from './views/AnalysisResult.vue';
@@ -17,36 +12,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    },
-    {
-      path: '/tsutomu',
-      name: 'tsutomu',
-      component: Tsutomu
-    },
-    {
-      path: '/toki',
-      name: 'toki',
-      component: Toki
-    },
-    {
-      path: '/minokich',
-      name: 'minokich',
-      component: Minokich
-    },
-    {
-      path: '/naoto',
-      name: 'naoto',
-      component: Naoto
+      name: 'analysisResultList',
+      component: AnalysisResultList
     },
     {
       path: '/analysisRequest',
